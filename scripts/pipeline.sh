466fb02386f414e78fc04794ec1dcfadc1e408fc
thetaDEM <137138315+DEMest@users.noreply.github.com>
2026-08-22 17:23:41 +0700

    ci: перегенерация артефактов после правки конфига

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-# generated-from-config: sha256:a15da37c98be5548f28016cd704d75ffbbacb62e649847fb36d7365debde36fd
+# generated-from-config: sha256:d4211995483a45b9360db5286114d44f5dd80f8a57bd2218180c2192e706f189
 # Файл сгенерирован из .pipeline/config.yml. Правки затрутся при следующей генерации:
 # меняйте .pipeline/config.yml и перезапускайте генерацию.
 set -eu
